fb1a0ef456944d456aa872811763613f61d1141a
sniperwang <tengwang1319@gmail.com>
2020-03-27 23:43:12 +0800

    mybatis demo

---
+++ mybatis/src/com/mybatis/test/TestMybatis.java
@@ -0,0 +1,26 @@
+package com.mybatis.test;
+
+import com.mybatis.pojo.Category;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.InputStream;
+import java.util.List;
+
+public class TestMybatis {
+    public static void main(String[] args) throws Exception {
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        List<Category> categories = sqlSession.selectList("listCategory");
+        for (Category c : categories) {
+            System.out.println(c.getName());
+        }
+        sqlSession.commit();
+        sqlSession.close();
+    }
+}
